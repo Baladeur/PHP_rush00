f7a6e687432213b34ef944c2287c1cb74c364b6f
yixuan WANG <yixuanamelia@gmail.com>
2019-10-20 20:38:37 +0200

    logout added

--- components/navbar.php
+++ components/navbar.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <div class="navBarStyle">
     <ul>
         <li><a class="active" href="#home">Home</a></li>
@@ -9,8 +12,17 @@
                 <a href="#">Category 3</a>
             </div>
         </li>
-        <li style="float:right"><a href="/login.php">Login</a></li>
-        <li style="float:right"><a href="/register.php">Register</a></li>
+        <?php
+            if (isset($_SESSION['loggued_on_user']) && $_SESSION('loggued_on_user') != NULL) {
+                echo"
+                <li style=\"float:right\"><a href=\"/logout.php\">Logout</a></li>";
+                echo "<script>alert('ddd')</script>";
+            } else {
+                echo "
+                <li style=\"float:right\"><a href=\"/login.php\">Login</a></li>
+                <li style=\"float:right\"><a href=\"/register.php\">Register</a></li>";
+            } 
+            ?>
         <li style="float:right"><a href="/basket.php">Shopping Cart</a></li>
     </ul>
 </div>
